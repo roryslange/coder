08205265499da646423bc4090a72ccfea334474a
Rory Lange <rorystrattonlange@gmail.com>
2025-12-20 00:03:31 -0500

    use pointer recievers instead of new objects

--- interfaces/open_file_tui.go
+++ interfaces/open_file_tui.go
@@ -1,7 +1,6 @@
 package interfaces
 
 import (
-	"fmt"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
@@ -9,30 +8,37 @@ import (
 
 type openFilePage struct {
 	filePath string
-	file os.File
+	file *os.File
 }
 
-func OpenFile(path string) openFilePage {
-	return openFilePage{filePath: path}
+func OpenFile(path string) *openFilePage {
+	return &openFilePage{filePath: path}
 }
 
-func (o openFilePage) Init() tea.Cmd { 
+func (o *openFilePage) Init() tea.Cmd { 
 	file, err := os.OpenFile(o.filePath, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		panic(err)
 	}
-	o.file = *file
-	fmt.Println("yup file is here: " + o.filePath)
+	o.file = file
+
 	return nil 
 }
 
-func (o openFilePage) View() string { return "file" }
+func (o *openFilePage) View() string {
+	return ""
+}
 
-func (o openFilePage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func (o *openFilePage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
     switch msg.(type) {
     case tea.KeyMsg:
         switch msg.(tea.KeyMsg).String() {
         case "ctrl+c":
+			e := o.file.Close()
+			if e != nil {
+				panic(e)
+			}
+
             return o, tea.Quit
         }
     }
